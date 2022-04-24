58c98d380828ff4c82bb5ca85b804c9c7eef3fe3
Steffen Bruchmann <github@sbruchmann.me>
2022-04-24 16:42:03 +0200

    Add trailing slash to paths and URLs

--- install.sh
+++ install.sh
@@ -11,13 +11,13 @@ ln --symbolic ~/dotfiles/git/attributes ~/.gitattributes
 ln --symbolic ~/dotfiles/git/config ~/.gitconfig
 ln --symbolic ~/dotfiles/git/ignore ~/.gitignore
 
-mkdir -p ~/.config/herbstluftwm
+mkdir -p ~/.config/herbstluftwm/
 ln --symbolic ~/dotfiles/herbstluftwm/autostart ~/.config/herbstluftwm/autostart
 
-mkdir -p ~/.config/neovim
+mkdir -p ~/.config/neovim/
 ln --symbolic ~/dotfiles/neovim/init.vim ~/.config/neovim/init.vim
 
 mkdir -p ~/.config/tmux/
 ln --symbolic ~/dotfiles/tmux.conf ~/.config/tmux/tmux.conf
 mkdir -p ~/.config/tmux/plugins/
-git clone https://github.com/tmux-plugins/tpm ~/.config/tmux/plugins/tpm/
+git clone https://github.com/tmux-plugins/tpm/ ~/.config/tmux/plugins/tpm/
